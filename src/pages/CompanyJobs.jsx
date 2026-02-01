4c684eba5ad295d0ff5b6164c9edce95d1242577
Aslan Baishuak <aslanbaishuak0@gmail.com>
2026-02-01 15:15:32 +0500

    Full project

--- src/pages/CompanyJobs.jsx
+++ src/pages/CompanyJobs.jsx
@@ -42,7 +42,6 @@ const CompanyJobs = () => {
     try {
       await updateJob(editingJob);
       setJobList(updatedJobs);
-      localStorage.setItem("jobs", JSON.stringify(updatedJobs)); // Remove unnused
       setIsModalOpen(false);
     } catch (err) {
       console.error("Failed to save job edits:", err);
@@ -58,7 +57,6 @@ const CompanyJobs = () => {
     try {
       await deleteJob(id);
       setJobList(updatedJobs);
-      localStorage.setItem("jobs", JSON.stringify(updatedJobs)); // Remove unnused
     } catch (err) {
       console.error("Failed to delete job:", err);
     }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -18,7 +18,10 @@ const Register = () => {
       setError("Password must be at least 5 characters long.");
       return false;
     }
-    // check if email is empty
+    if (email.trim() === "") {
+      setError("Email is required.");
+      return false;
+    }
     if (!role) {
       setError("Please select a role.");
       return false;
@@ -36,7 +39,7 @@ const Register = () => {
         const data = await registerUser(name, email, password, role);
         console.log("Registration successful:", data);
         alert("Account created successfully!");
-        // if success, go to login page
+        window.location.replace("/login");
       } catch (error) {
         console.error("Registration error:", error);
         setError("Registration failed. Please try again.");
